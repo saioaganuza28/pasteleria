f81534c95d64ebbcd222f47aa4224164573ea581
mariaherreroarbizu <mherreroarbizu@gmail.com>
2025-03-21 19:59:52 +0100

    footer

--- pasteleriaGoxo/src/Componentes/ui/Footer.jsx
+++ pasteleriaGoxo/src/Componentes/ui/Footer.jsx
@@ -13,6 +13,22 @@ function Footer() {
             <p>Tel: +34 600 123 456</p>
           </div>
 
+          {/* Mapa interactivo */}
+        <div className="footer-section">
+          <h5>Ubicación</h5>
+          <div className="map-container">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3038.235246297281!2d-74.00601538459446!3d40.71277577933143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a292c9a5b5f%3A0x3ee12bfa479d032d!2sOne%20World%20Trade%20Center!5e0!3m2!1ses-419!2smx!4v1617776420109!5m2!1ses-419!2smx"
+              width="100%"
+              height="150"
+              style={{border: 0}}
+              allowFullScreen=""
+              loading="lazy"
+              title="Ubicación de Pastelitos Store"
+            ></iframe>
+          </div>
+        </div>
+
 
           {/* Redes sociales */}
           <div className="footer-section">
