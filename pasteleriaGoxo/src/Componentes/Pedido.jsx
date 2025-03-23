ad7662c17ea463203f5a9c2c31e107914d151bc4
saioaganuza28 <sganuzaado1@gmail.com>
2025-03-23 17:44:36 +0100

    cambios

--- pasteleriaGoxo/src/Componentes/Pedido.jsx
+++ pasteleriaGoxo/src/Componentes/Pedido.jsx
@@ -4,45 +4,6 @@ import './Pedido.css'
 import { Link } from 'react-router';
 
 function Pedido(props) {
-    const [precios, setPrecios] = useState({});
-    const [productosFirebase, setProductosFirebase] = useState([]);
-    const [productosListos, setProductosListos] = useState(false);
-
-    const productos = props.pedido.productos || {};
-
-    const cantidadProductos = Object.values(productos).reduce((acc, cantidad) => acc + cantidad, 0);
-
-    const totalPrecio = productosListos ? Object.entries(productos).reduce((acc, [nombre, cantidad]) => {
-        const precioProducto = precios[nombre] || 0;
-        return acc + (precioProducto * cantidad);
-    }, 0) : 0;
-
-    useEffect(() => {
-        axios.get('https://goxopasteleria-default-rtdb.europe-west1.firebasedatabase.app/productos.json')
-            .then((response) => {
-                const preciosObtenidos = {};
-                const productosArray = [];
-                for (let key in response.data) {
-                    const producto = response.data[key];
-                    productosArray.push({
-                        id: key,
-                        imagen: producto.imagen,
-                        nombre: producto.nombre,
-                        precio: producto.precio,
-                        valoracion: producto.valoracion,
-                        descripcion: producto.descripcion,
-                        clave: producto.clave,
-                    });
-                    preciosObtenidos[producto.nombre] = producto.precio;
-                }
-                setProductosFirebase(productosArray);
-                setPrecios(preciosObtenidos);
-                setProductosListos(true);
-            })
-            .catch((error) => {
-                console.log('Error al obtener los precios:', error);
-            });
-    }, []);
 
     return (
         <>
@@ -63,18 +24,6 @@ function Pedido(props) {
                 </Card>
             </Container>
         </>
-    );
+    )
 }
-
-export default Pedido;  //hola
-
-
-
-
-
-
-
-
-
-
-
+export default Pedido;
